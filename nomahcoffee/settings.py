94a777dac4c263333cccc60ec555ca1b55ce2704
Caleb Rudnicki <calebrudnicki@caleb.myfiosgateway.com>
2021-08-03 17:10:34 -0400

    Set dev db to be sqlite and prod db to be postresql

--- nomahcoffee/settings.py
+++ nomahcoffee/settings.py
@@ -86,7 +86,11 @@ AUTH_USER_MODEL = 'authapp.User'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'dev': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'prod': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('DATABASE_NAME'),
         'USER': config('DATABASE_USER'),
@@ -96,6 +100,7 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = DATABASES['dev' if DEBUG else 'prod']
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
